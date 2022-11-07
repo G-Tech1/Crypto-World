ba95aa0d8955d5373adc8485dfdc8e03dfa23e39
Gilan <gilserrant5@gmail.com>
2022-11-06 23:12:56 -0500

    added notes

--- cryptoapp/src/app/store.js
+++ cryptoapp/src/app/store.js
@@ -4,9 +4,12 @@ import { cryptoApi } from '../services/cryptoApi';
 
 export default configureStore({
   reducer: {
+    // Add the generated reducer as a specific top-level slice
     [cryptoApi.reducerPath]: cryptoApi.reducer,
     // needs to be specified for every reducer thats created
   },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(cryptoApi.middleware), //just add reducer being used
 });
